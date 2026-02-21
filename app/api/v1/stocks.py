8a508bd3e4134cac6f15cabad6a6db58bfe1de23
Anwar Hermuche <anwarhermuche2@gmail.com>
2026-02-21 17:19:34 -0300

    docker things updated

--- app/api/v1/stocks.py
+++ app/api/v1/stocks.py
@@ -7,6 +7,6 @@ from app.schemas.stocks import StocksReponseSchema, StocksRequestSchema
 router = APIRouter(prefix = "/api/v1/stocks", tags = ["Stocks"])
 
 
-@router.get(path = "/analyze", response_model = StocksReponseSchema)
+@router.post(path = "/analyze", response_model = StocksReponseSchema)
 def analyze_stock(request: StocksRequestSchema):
     return StocksReponseSchema(**Report(ticker = request.ticker).get().model_dump())
